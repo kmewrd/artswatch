d2ca8bf2394e14324f8c9c1eea0a79937699bcbe
Kim Ward <kmewrd@outlook.com>
2022-06-06 18:39:09 -0500

    Use hook to fetch articles and map to return Articles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import './App.css'
+import ArticleList from './components/ArticleList/ArticleList'
 
 const App = () => {
   return (
     <div className="App">
-      
+      <ArticleList />
     </div>
   );
 }
--- src/components/ArticleList/ArticleList.js
+++ src/components/ArticleList/ArticleList.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import fetchArticles from '../../apiCalls'
+import Article from '../Article/Article'
 
 const ArticleList = () => {
+  const [list, setList] = useState([])
+
+  useEffect(() => {
+    fetchArticles().then(data => setList(data.results))
+  }, [])
+
+  // useEffect(() => {
+  //   console.log(list)
+  // }, [list])
+
+  const articles = list.map(article => <Article key={article.uri} />)
+  
   return (
     <div>
-
+      {articles}
     </div>
   )
 }
